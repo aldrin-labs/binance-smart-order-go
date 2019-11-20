eaf1f22cac0679f9072109bbd6f020e687f0f21e
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-11-20 21:24:55 +0100

    Code formatting fix by gofmt

--- src/service/strategy.go
+++ src/service/strategy.go
@@ -1,27 +1,26 @@
 package service
 
 import (
-    "gitlab.com/crypto_project/core/strategy_service/src/sources/mongodb/models"
-    "gitlab.com/crypto_project/core/strategy_service/src/trading"
-    "go.mongodb.org/mongo-driver/mongo"
+	"gitlab.com/crypto_project/core/strategy_service/src/sources/mongodb/models"
+	"gitlab.com/crypto_project/core/strategy_service/src/trading"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Strategy object
 type Strategy struct {
-    model models.MongoStrategy
+	model models.MongoStrategy
 }
 
 func GetStrategy(cur *mongo.Cursor) (*Strategy, error) {
-    result := &models.MongoStrategy{}
-    err := cur.Decode(&result)
-    return &Strategy{model:models.MongoStrategy{}}, err
+	result := &models.MongoStrategy{}
+	err := cur.Decode(&result)
+	return &Strategy{model: models.MongoStrategy{}}, err
 }
 
-func (strategy * Strategy) Start() {
+func (strategy *Strategy) Start() {
 
 }
 
-
-func (strategy * Strategy) CreateOrder(rawOrder trading.CreateOrderRequest) {
-    trading.CreateOrder(rawOrder)
+func (strategy *Strategy) CreateOrder(rawOrder trading.CreateOrderRequest) {
+	trading.CreateOrder(rawOrder)
 }
