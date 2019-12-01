b0eea7fc70c4ea41e91ae15dfbf17efa87db0e2d
Igor Limansky <i.e.limansky@gmail.com>
2019-12-01 15:01:33 +0300

    implement redis, add redis test

--- go.mod
+++ go.mod
@@ -3,13 +3,11 @@ module gitlab.com/crypto_project/core/strategy_service
 go 1.13
 
 require (
-	github.com/go-redis/redis v6.15.2+incompatible
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/google/go-cmp v0.3.0 // indirect
 	github.com/joho/godotenv v1.3.0
-	github.com/onsi/ginkgo v1.8.0 // indirect
-	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/qmuntal/stateless v1.1.0
 	github.com/tidwall/pretty v0.0.0-20190325153808-1166b9ac2b65 // indirect
 	github.com/valyala/fasthttp v1.3.0
--- src/sources/redis/redisLoop.go
+++ src/sources/redis/redisLoop.go
@@ -1,9 +1,83 @@
 package redis
 
+import (
+	"context"
+	"encoding/json"
+	"gitlab.com/crypto_project/core/strategy_service/src/service/strategies"
+	"sync"
+)
+
 type RedisLoop struct {
-	pairsToCallbacks
+	OhlcvMap sync.Map // <string: exchange+pair+o/h/l/c/v, OHLCV: ohlcv>
+}
+var redisLoop *RedisLoop
+
+func InitRedis() {
+	if redisLoop == nil {
+		redisLoop = &RedisLoop{}
+		redisLoop.SubscribeToPairs()
+	}
 }
 
-func StartRedisLoop() {
+func GetPrice(pair, exchange string) *strategies.OHLCV {
+	if redisLoop == nil {
+		redisLoop = &RedisLoop{}
+	}
+	return redisLoop.GetPrice(pair, exchange)
+}
 
+type OrderbookOHLCV struct {
+	Open float64 `json:"open_price,float"`
+	High float64 `json:"high_price,float"`
+	Low float64 `json:"low_price,float"`
+	Close float64 `json:"close_price,float"`
+	Volume float64 `json:"volume,float"`
+	Base string `json:"fsym,string"`
+	Quote string `json:"tsym,string"`
+	Exchange string `json:"volume,string"`
+}
+
+func (rl *RedisLoop) SubscribeToPairs() {
+	go ListenPubSubChannels(context.TODO(), func() error {
+		return nil
+	}, func(channel string, data []byte) error {
+		go rl.UpdateOHLCV(channel, data)
+		return nil
+	}, "*:*:60")
+}
+
+func (rl *RedisLoop) UpdateOHLCV(channel string, data []byte) {
+	ohlcvOB := OrderbookOHLCV{}
+	_ = json.Unmarshal(data, &ohlcvOB)
+	pair := ohlcvOB.Quote+"_"+ohlcvOB.Base
+	exchange := ohlcvOB.Exchange
+	ohlcv := strategies.OHLCV{
+		Open:   ohlcvOB.Open,
+		High:   ohlcvOB.High,
+		Low:    ohlcvOB.Low,
+		Close:  ohlcvOB.Close,
+		Volume: ohlcvOB.Volume,
+	}
+	rl.OhlcvMap.Store(exchange+pair, ohlcv)
+
+}
+func (rl *RedisLoop) FillPair(pair, exchange string) *strategies.OHLCV {
+	redisClient := GetRedisClientInstance(false, true, false)
+	baseStr := pair + ":0:" + exchange + ":60:"
+	ohlcvResultArr, _ := redisClient.Do("GET", baseStr+"o", baseStr+"h", baseStr+"l", baseStr+"c", baseStr+"v")
+
+	responseArr := ohlcvResultArr.([]interface{})
+	for _, value := range responseArr {
+		println(value)
+	}
+	return nil
+}
+
+func (rl *RedisLoop) GetPrice(pair, exchange string) *strategies.OHLCV  {
+	ohlcvRaw, ob := rl.OhlcvMap.Load(pair+exchange)
+	if ob {
+		ohlcv := ohlcvRaw.(strategies.OHLCV)
+		return &ohlcv
+	}
+	return nil
 }
