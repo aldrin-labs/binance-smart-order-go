49b1847b6d72c96ed8ccdcfea1c46f5335971ae0
EUsipov <usupovzh@gmail.com>
2020-03-09 21:21:06 +0300

    add interface methods to redis loop

--- src/sources/redis/redisLoop.go
+++ src/sources/redis/redisLoop.go
@@ -3,14 +3,16 @@ package redis
 import (
 	"context"
 	"encoding/json"
-	"gitlab.com/crypto_project/core/strategy_service/src/service/interfaces"
 	"strconv"
 	"sync"
+
+	"gitlab.com/crypto_project/core/strategy_service/src/service/interfaces"
 )
 
 type RedisLoop struct {
 	OhlcvMap sync.Map // <string: exchange+pair+o/h/l/c/v, OHLCV: ohlcv>
 }
+
 var redisLoop *RedisLoop
 
 func InitRedis() interfaces.IDataFeed {
@@ -31,15 +33,15 @@ func (rl *RedisLoop) GetPriceForPairAtExchange(pair string, exchange string, mar
 }
 
 type OrderbookOHLCV struct {
-	Open float64 `json:"open_price,float"`
-	High float64 `json:"high_price,float"`
-	Low float64 `json:"low_price,float"`
-	MarketType int64 `json:"market_type,float"`
-	Close float64 `json:"close_price,float"`
-	Volume float64 `json:"volume,float"`
-	Base string `json:"tsym"`
-	Quote string `json:"fsym"`
-	Exchange string `json:"exchange"`
+	Open       float64 `json:"open_price,float"`
+	High       float64 `json:"high_price,float"`
+	Low        float64 `json:"low_price,float"`
+	MarketType int64   `json:"market_type,float"`
+	Close      float64 `json:"close_price,float"`
+	Volume     float64 `json:"volume,float"`
+	Base       string  `json:"tsym"`
+	Quote      string  `json:"fsym"`
+	Exchange   string  `json:"exchange"`
 }
 
 func (rl *RedisLoop) SubscribeToPairs() {
@@ -54,7 +56,7 @@ func (rl *RedisLoop) SubscribeToPairs() {
 func (rl *RedisLoop) UpdateOHLCV(channel string, data []byte) {
 	var ohlcvOB OrderbookOHLCV
 	_ = json.Unmarshal(data, &ohlcvOB)
-	pair := ohlcvOB.Quote+"_"+ohlcvOB.Base
+	pair := ohlcvOB.Quote + "_" + ohlcvOB.Base
 	exchange := "binance"
 	ohlcv := interfaces.OHLCV{
 		Open:   ohlcvOB.Open,
@@ -79,10 +81,20 @@ func (rl *RedisLoop) FillPair(pair, exchange string) *interfaces.OHLCV {
 }
 
 func (rl *RedisLoop) GetPrice(pair, exchange string, marketType int64) *interfaces.OHLCV {
-	ohlcvRaw, ob := rl.OhlcvMap.Load(exchange+pair+strconv.FormatInt(marketType, 10))
+	ohlcvRaw, ob := rl.OhlcvMap.Load(exchange + pair + strconv.FormatInt(marketType, 10))
 	if ob == true {
 		ohlcv := ohlcvRaw.(interfaces.OHLCV)
 		return &ohlcv
 	}
 	return nil
+}
+
+func (rl *RedisLoop) GetTickerData() []interfaces.OHLCV {
+	panic("implement me")
+	return []interfaces.OHLCV{}
+}
+
+func (rl *RedisLoop) GetCurrentTick() int {
+	panic("implement me")
+	return 0
 }
