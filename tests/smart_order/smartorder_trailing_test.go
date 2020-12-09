59d84bd4356e6a650379cb94d7175cee09794027
Alisher A. Khassanov <a.khssnv@gmail.com>
2020-12-09 15:25:12 +0400

    Small fix for a test

--- tests/smart_order/smartorder_trailing_test.go
+++ tests/smart_order/smartorder_trailing_test.go
@@ -191,12 +191,13 @@ func TestSmartOrderTrailingEntryAndFollowTrailingMaximumsWithoutEarlyExitWithHig
 	tradingApi := tests.NewMockedTradingAPI()
 	tradingApi.BuyDelay = 100
 	tradingApi.SellDelay = 20100
-	strategy := strategies.Strategy{
-		Model: &smartOrderModel,
-	}
-	keyId := primitive.NewObjectID()
 	//sm := mongodb.StateMgmt{}
 	sm := tests.NewMockedStateMgmt(tradingApi, df)
+	strategy := strategies.Strategy{
+		Model: &smartOrderModel,
+		StateMgmt: &sm,
+	}
+	keyId := primitive.NewObjectID()
 	smartOrder := smart_order.NewSmartOrder(&strategy, df, tradingApi, strategy.Statsd, &keyId, &sm)
 	smartOrder.State.OnTransitioned(func(context context.Context, transition stateless.Transition) {
 		log.Print("transition: source ", transition.Source.(string), ", destination ", transition.Destination.(string), ", trigger ", transition.Trigger.(string), ", isReentry ", transition.IsReentry())
