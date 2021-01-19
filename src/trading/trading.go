f5bbc365748f3d70a007312c934c45f9aeb72b1d
Alisher A. Khassanov <a.khssnv@gmail.com>
2021-01-19 16:21:42 +0400

    Extend trading request timeout

--- src/trading/trading.go
+++ src/trading/trading.go
@@ -77,13 +77,13 @@ func InitTrading() ITrading {
 // Request encodes data to JSON, sends it to exchange service and returns decoded response.
 func Request(method string, data interface{}) interface{} {
 	url := "http://" + os.Getenv("EXCHANGESERVICE") + "/" + method
-	log.Info("request", zap.String("url", url))
+	log.Info("request", zap.String("url", url), zap.String("data", fmt.Sprintf("%+v", data)))
 
 	var jsonStr, err = json.Marshal(data)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: 30 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
 		retryDelay := time.Duration(1 * time.Second)
@@ -91,6 +91,8 @@ func Request(method string, data interface{}) interface{} {
 			zap.String("url", url),
 			zap.Error(err),
 			zap.Duration("retry in, seconds", retryDelay),
+			zap.String("request", fmt.Sprintf("%+v", req)),
+			zap.String("data", fmt.Sprintf("%+v", data)),
 		)
 		time.Sleep(retryDelay)
 		return Request(method, data)
