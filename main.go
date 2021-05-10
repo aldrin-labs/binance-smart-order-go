c590dec22dfa418ce29ff428997494fb8a952cb8
Alisher A. Khassanov <a.khssnv@gmail.com>
2021-05-10 15:50:32 +0400

    Brute shutdown by SIGTERM and SIGINT

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"github.com/joho/godotenv"
 	"gitlab.com/crypto_project/core/strategy_service/src/server"
 	"gitlab.com/crypto_project/core/strategy_service/src/service"
 	"log"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
 func init() {
@@ -17,6 +20,15 @@ func init() {
 }
 
 func main() {
+	ctx := context.Background()
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGTERM, syscall.SIGINT) // k8s sends SIGTERM and waits
+	defer stop()
+	go func() {
+		<-ctx.Done()
+		log.Println("Brute shutdown.")
+		os.Exit(0)
+	}()
+
 	var wg sync.WaitGroup // TODO: init top-level context
 	wg.Add(1)
 	go server.RunServer(&wg)
